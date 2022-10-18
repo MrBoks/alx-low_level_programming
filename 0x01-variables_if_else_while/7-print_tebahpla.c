a7b943ead1a84f00b0214fbaf62612f4dc16ecd6
MrBoks <get2noahodogg@yahoo.com>
2022-10-18 03:36:16 +0000

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabets in reverse order
+ * Return: Always 0
+ */
+int main(void)
+{
+	char rv;
+
+	for (rv = 'z; rv >= 'a'; rv--)
+		putchar(rv);
+
+	putchar('\n');
+
+	return (0);
+}
