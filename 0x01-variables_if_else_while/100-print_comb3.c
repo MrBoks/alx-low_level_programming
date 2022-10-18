490af9c708ad1fe5a1c8453a53a75a3957952839
MrBoks <get2noahodogg@yahoo.com>
2022-10-18 17:50:45 +0000

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,23 +6,23 @@
  */
 int main(void)
 {
-        int num1, num2;
-        num1=48;
+	int num1, num2;
 
-        while (num1 < 58)
-        {
-                num2=48;
-                while (num2 < 58)
-                {
-                        putchar(num1);
-                        putchar(num2);
-                        num2++;
-                        putchar(',');
-                        putchar(' ');
-                }
-                num1++;
-        }
-        putchar("\n");
+	num1 = 48;
+	while (num1 < 58)
+	{
+		num2 = 48;
+		while (num2 < 58)
+		{
+			putchar(num1);
+			putchar(num2);
+			num2++;
+			putchar(',');
+			putchar(' ');
+		}
+		num1++;
+	}
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
