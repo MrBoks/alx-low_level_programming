f3b10c2dac3b8afa12ee44e0f467cce6052e409c
MrBoks <get2noahodogg@yahoo.com>
2022-10-18 18:58:12 +0000

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,18 +6,17 @@
  */
 int main(void)
 {
-	int num1, num2;
+	int num1 = '0';
+	int num2 = '0';
 
-	num1 = 48;
-	while (num1 < 58)
+	while (num2 <= 9)
 	{
-		num2 = 48;
-		while (num2 < 58)
+		while (num1 <= 9)
 		{
 			if (!((num1 == num2) || (num2 > num1)))
 			{
-			putchar(num1);
 			putchar(num2);
+			putchar(num1);
 			num2++;
 				if (!(num1 == '9' && num2 == '8'))
 				{
