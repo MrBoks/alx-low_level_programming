e493e90704fea5dde74952c12042b1c87439ddee
MrBoks <get2noahodogg@yahoo.com>
2022-10-29 04:01:36 +0000

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,10 +16,10 @@ char *cap_string(char *str)
 		if (str[i - 1] == ' ' ||
 		    str[i - 1] == ' ' ||
 		    str[i - 1] == '\t' ||
-	            str[i - 1] == '\n' ||
+		    str[i - 1] == '\n' ||
 		    str[i - 1] == ',' ||
 		    str[i - 1] == ';' ||
-	            str[i - 1] == '.' ||
+		    str[i - 1] == '.' ||
 		    str[i - 1] == '!' ||
 		    str[i - 1] == '?' ||
 		    str[i - 1] == '"' ||
