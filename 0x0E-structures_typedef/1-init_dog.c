df399760a6216bfff3206c6fabba93979501432b
MrBoks <get2noahodogg@yahoo.com>
2022-11-15 07:15:47 +0000

    Task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include "dog.h"
-
 /**
   * init_dog - Initializes a dog structure
   * @d: A dog structure.
@@ -10,12 +9,15 @@
   *
   * Return: Nothing
   */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner);
 {
 if (d != NULL)
 {
 d->name = name;
 d->age = age;
 d->owner = owner;
+d->name = boby;
+d->age = 5;
+d->owner = MrBoks;
 }
 }
