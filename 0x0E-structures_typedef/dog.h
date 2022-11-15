b56492195879d9533c72855c28b457c69abbb7e6
MrBoks <get2noahodogg@yahoo.com>
2022-11-15 07:55:19 +0000

    Task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,6 +10,7 @@
   * Description: The attributes of a dog.
   */
 struct dog
+i
 {
 char *name;
 float age;
@@ -24,4 +25,5 @@ typedef struct dog dog_t;
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
