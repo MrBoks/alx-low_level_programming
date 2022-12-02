ea2882b7b6224184c32c88ade110a0b20e46c409
MrBoks <get2noahodogg@yahoo.com>
2022-12-02 23:58:37 +0000

    Task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,6 +15,6 @@ if (d != NULL)
 printf("Name: %s\n", (d->name) ? d->name : "(nil)");
 printf("Age: %f\n", (d->age) ? d->age : 0);
 printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
-[O
+
 }
 }
